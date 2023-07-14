c2105f6e10c75de753d7b973e5dc080ce30dafbe
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-07-14 13:08:17 +0200

    foutje in de letter en cijfer check bij age en name oplossen en de overige validaties uitgewerkt

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import {useForm} from "react-hook-form";
 
 function App() {
-    const {handleSubmit, formState: {errors}, register} = useForm({mode: 'onChange'});
+    const {handleSubmit, formState: {errors, isDirty, isValid}, register, watch} = useForm({mode: 'onChange'});
+
+    const watchDeliveryFrequency = watch('deliveryFrequency');
 
     function handleFormSubmit(data) {
         console.log(data);
@@ -25,7 +27,7 @@ function App() {
                             message: 'Dit veld is verplicht',
                         },
                         validate: {
-                            matchPattern: (v) => /^[a-zA-Z]$/.test(v) || "dit veld mag geen cijfers bevatten",
+                            matchPattern: (v) => /^[a-zA-Z]+$/.test(v) || "dit veld mag geen cijfers bevatten",
                             minLength: (v) => v['length'] >= 2 || "vul een geldige naam in",
                             maxLength: (v) => v['length'] <= 50 || "het maximale aantal karakters is bereikt",
                         }
@@ -44,7 +46,7 @@ function App() {
                             message: 'Dit veld is verplicht',
                         },
                         validate: {
-                            matchPattern: (v) => /^[a-zA-Z]$/.test(v) || "dit veld mag geen cijfers bevatten",
+                            matchPattern: (v) => /^[a-zA-Z]+$/.test(v) || "dit veld mag geen cijfers bevatten",
                             minLength: (v) => v['length'] >= 2 || "vul een geldige naam in",
                             maxLength: (v) => v['length'] <= 60 || "het maximale aantal karakters is bereikt",
                         }
@@ -63,7 +65,7 @@ function App() {
                             message: 'Dit veld is verplicht',
                         },
                         validate: {
-                            matchPattern: (v) => /^[0-9]$/.test(v) || "dit veld mag geen letters bevatten",
+                            matchPattern: (v) => /^[0-9]+$/.test(v) || "dit veld mag geen letters bevatten",
                             min: (v) => v >= 18 || "je moet minimaal 18 jaar oud zijn",
                         }
                     })}
@@ -95,31 +97,36 @@ function App() {
                     id="delivery-frequency-selector"
                     {...register("deliveryFrequency", {
                         required: {
-                        value: true,
-                        message: 'Dit veld is verplicht',
-                    },
+                            value: true,
+                            message: 'Dit veld is verplicht',
+                        },
                         validate: {
-                        value: (v) => v !== "Select..." || "selecteer een bezorgfrequentie",
-                    }
+                            value: (v) => v !== "Select..." || "selecteer een bezorgfrequentie",
+                        }
                     })}
                 >
                     <option value="Select...">Select...</option>
-                    <option value="iedere week">iedere week</option>
-                    <option value="om de week">om de week</option>
-                    <option value="iedere maand">iedere maand</option>
-                    <option value="iedere maand">anders</option>
+                    <option value="weekly">iedere week</option>
+                    <option value="every-other-week">om de week</option>
+                    <option value="monthly">iedere maand</option>
+                    <option value="other">anders</option>
                 </select>
                 {errors.deliveryFrequency && <small>{errors.deliveryFrequency.message}</small>}
-                <label htmlFor="custom-frequency-textarea">
-                    Specificeer andere bezorgfrequentie:
-                </label>
-                <textarea
-                    {...register("customDeliveryFrequency")}
-                    rows="1"
-                    cols="50"
-                    maxLength="50"
-                >
-                        </textarea>
+
+                {watchDeliveryFrequency === "other" &&
+                    <label htmlFor="custom-frequency-textarea">
+                        Specificeer andere bezorgfrequentie:
+                    </label>
+                }
+                {watchDeliveryFrequency === "other" &&
+                    <textarea
+                        {...register("customDeliveryFrequency")}
+                        rows="1"
+                        cols="50"
+                        maxLength="50"
+                    >
+                </textarea>
+                }
                 <label htmlFor="remarks-textarea">
                     Opmerking:
                 </label>
@@ -135,16 +142,30 @@ function App() {
                     <input
                         type="checkbox"
                         id="conditions-checkbox"
-                        {...register("conditions")}
+                        {...register("conditions", {
+                            required: {
+                                checked: true,
+                                message: 'Dit veld is verplicht',
+                            },
+                            validate: {
+                                checked: (v) => v || "ga akkoord met de algemene voorwaarden om te verzenden",
+                            }
+                        })}
                     />
+                    {errors.zipcode && <small>{errors.zipcode.message}</small>}
                     Akkoord met de algemene voorwaarden.
                 </label>
-                <button type="submit" id="send-form-button">
+                <button
+                    type="submit"
+                    id="send-form-button"
+                    disabled={!isDirty || !isValid}
+                >
                     Verzenden
                 </button>
             </form>
         </>
-    );
+    )
+        ;
 }
 
 export default App;
